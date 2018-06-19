6795b9a4bf759f99abfa31e6e07a5299960f8572
Jully Motta <jmotta@redhat.com>
2018-06-19 17:14:59 -0300

    padronizacao de metodos e parametros de retorno

--- client.py
+++ client.py
@@ -3,10 +3,26 @@
 import xmlrpclib
 
 def main():
-    print "This is a client" 
+    print "Esse eh o cliente!" 
 
     client = xmlrpclib.ServerProxy("http://localhost:8080")
-    print client.add(10,20)
+    
+    #variavel para metodo EnviaIntRecebeInt
+    valor = int(10)
+
+    #variaveis para metodos EnviaLongRecebeLong e EnviaLong8RecebeLong
+    x = 10
+    y = 20
+    z = 30
+    w = 40
+    a = 50
+    b = 60 
+    c = 70
+    d = 85
+    print client.EnviaIntRecebeInt(valor)
+    print client.EnviaLongRecebeLong(x)
+    print client.EnviaLong8RecebeLong(x,y,z,w,a,b,c,d)
 
 if __name__ == "__main__":
     main()
+ 
--- server.py
+++ server.py
@@ -2,14 +2,38 @@
 
 import SimpleXMLRPCServer
 
-def add(a,b):
-    return a+b
+#Cenario onde nao passamos nenhum argumento para uma funcao Void
+def VoidMetodoSemParametros():
+    valor = 10 + 20
+
+#Cenario onde passamos um Integer e recebemos um Integer
+def EnviaIntRecebeInt(x):
+    valor = x + 10
+    valor = int(valor)
+    return valor
+
+#Cenrio onde passamos um Long e recebemos um Long
+def EnviaLongRecebeLong(x):
+    valor = x + 15
+    valor = long(valor)
+    return valor
+
+#Cenario onde passamos 8 variaveis Long e recebemos um Long
+def EnviaLong8RecebeLong(x, y, z, w, a, b, c, d):
+    valor = x + y + z + w + a + b + c + d
+    valor = long(valor)
+    return valor
+
+#Cenario onde passamos strings com valor de potencias de 10 MUITO grandes e recebemos uma string com uma frase
+#EnviaStringPotencia10RecebeString
 
 def main ():
-    print "This is a Server"
+    print "Esse eh o server"
     
     server = SimpleXMLRPCServer.SimpleXMLRPCServer(("0.0.0.0", 8080))
-    server.register_function(add)
+    server.register_function(EnviaIntRecebeInt)
+    server.register_function(EnviaLongRecebeLong)
+    server.register_function(EnviaLong8RecebeLong)
     print "Pressione CTRL + C para interromper..."
     server.serve_forever()
 
