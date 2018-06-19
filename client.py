80083a53e838cc381ab997e6d34c8992c2ae5cfa
Jully Motta <jmotta@redhat.com>
2018-06-19 19:09:47 -0300

    dados escritos em arquivo

--- client.py
+++ client.py
@@ -22,28 +22,32 @@ def main():
     
     #printa resultados e tempo de execucao
     
+    file = open("teste.txt", "w")
+
     for x in range(0, 20):
 
-    #start_time = time.time()
-    #result0 = client.VoidMetodoSemParametros()
-    #end_time = time.time()
-    #print "VoidMetodoSemParametros resultou em:",result0,"e rodou em",end_time-start_time
+        #start_time = time.time()
+        #result0 = client.VoidMetodoSemParametros()
+        #end_time = time.time()
+        #print "VoidMetodoSemParametros resultou em:",result0,"e rodou em",end_time-start_time
     
         start_time = time.time()
         result1 = client.EnviaIntRecebeInt(valor)
         end_time = time.time()
-        print "#EnviaIntRecebeInt levou",end_time-start_time,"milissegundos"
+        str_result1 = str(end_time-start_time)
+        file.write("#EnviaIntRecebeInt levou " + str_result1 + " milissegundos\n")
     
         start_time = time.time()
         result2 = client.EnviaLongRecebeLong(x)
         end_time = time.time()
-        print "#EnviaLongRecebeLong levou",end_time-start_time,"milissegundos"
-
+        str_result2 = str(end_time-start_time)
+        file.write("#EnviaLongRecebeLong levou " + str_result2 +" milissegundos\n")
     
         start_time = time.time()
         result3 = client.EnviaLong8RecebeLong(x,y,z,w,a,b,c,d)
         end_time = time.time()
-        print "#EnviaLong8RecebeLong levou",end_time-start_time,"milissegundos"
+        str_result3 = str(end_time-start_time)
+        file.write("#EnviaLong8RecebeLong levou " + str_result3 +" milissegundos\n")
 
 
 if __name__ == "__main__":
