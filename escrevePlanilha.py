49b6a49266f2805ed0833abdd7b7732e40c4eaab
Jonatas F Yano <jonatasyano@gmail.com>
2018-07-05 16:15:18 -0300

    Add files via upload

---
+++ escrevePlanilha.py
@@ -0,0 +1,34 @@
+import xlsxwriter 
+
+
+def main():
+    workbook = xlsxwriter.Workbook('dados_XML-RPC.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    arq = open('tempos_XML-RPC.txt', 'r')
+    texto = arq.readlines()
+    inicializaWorksheet(texto, worksheet)
+    row = 0
+    col = 0    
+    
+    for linha in texto :
+        if(row%21 == 0):
+            row = 0
+            col = col + 1
+        worksheet.write(row, col, linha.split(' ')[1].replace("\n", "").replace(".", ","))
+        row += 1
+    
+    arq.close()
+
+def inicializaWorksheet(texto, worksheet):
+    col = 0
+    row = 0
+    for linha in texto:
+        worksheet.write(row, col, linha.split(' ')[0])
+        row += 1
+        if(row == 21):
+            break
+
+if __name__ == "__main__":
+    main()
+ 
