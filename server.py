e9ce5f4484d74679bd7c43c2a58e96953bcd293e
Jonatas F Yano <jonatasyano@gmail.com>
2018-07-05 16:14:31 -0300

    Update server.py

--- server.py
+++ server.py
@@ -8,32 +8,38 @@ def VoidMetodoSemParametros():
 
 #Cenario onde passamos um Integer e recebemos um Integer
 def EnviaIntRecebeInt(x):
-    valor = x + 10
-    valor = int(valor)
+    valor = int(x)
     return valor
 
 #Cenrio onde passamos um Long e recebemos um Long
 def EnviaLongRecebeLong(x):
-    valor = x + 15
-    valor = long(valor)
-    return valor
+    valor = long(x)
+    return x
 
 #Cenario onde passamos 8 variaveis Long e recebemos um Long
 def EnviaLong8RecebeLong(x, y, z, w, a, b, c, d):
-    valor = x + y + z + w + a + b + c + d
-    valor = long(valor)
-    return valor
+    valor = long(x)
+    return x
 
 #Cenario onde passamos strings com valor de potencias de 10 MUITO grandes e recebemos uma string com uma frase
-#EnviaStringPotencia10RecebeString
+def EnviaStringPotencia10RecebeString(i):
+    return str(i)
+
+def EnviaArrayIntRecebeInt(dicionario):
+    return int(0)
+
+def EnviaArrayIntRecebeArray(dicionario):
+    return dicionario
 
 def main ():
     
     server = SimpleXMLRPCServer.SimpleXMLRPCServer(("0.0.0.0", 8080))
-#    server.register_function(VoidMetodoSemParametros)
     server.register_function(EnviaIntRecebeInt)
     server.register_function(EnviaLongRecebeLong)
     server.register_function(EnviaLong8RecebeLong)
+    server.register_function(EnviaStringPotencia10RecebeString)
+    server.register_function(EnviaArrayIntRecebeInt)
+    server.register_function(EnviaArrayIntRecebeArray)
     print "Pressione CTRL + C para interromper..."
     server.serve_forever()
 
